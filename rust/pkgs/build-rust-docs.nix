a36344692af54417648ebd8f84c5d62ac0a01ab0
h-banii <121690516+h-banii@users.noreply.github.com>
2026-06-26 04:10:06 +0000

    rust: pkgs: build-rust-docs: Fix --libs -> --lib flag

--- rust/pkgs/build-rust-docs.nix
+++ rust/pkgs/build-rust-docs.nix
@@ -50,6 +50,6 @@ craneLib.cargoDoc (
     pname = "${pname}-docs";
     src = filteredSource;
     inherit cargoArtifacts;
-    cargoExtraArgs = "-p ${pname} --libs --no-deps" + cargoExtraArgs;
+    cargoExtraArgs = "-p ${pname} --lib --no-deps" + cargoExtraArgs;
   }
 )
